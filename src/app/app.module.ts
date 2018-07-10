57f8fc94ab9f4d3deae49b78fe8ead8eb06ad68a
Tony <tmedinger@gmail.com>
2018-07-10 16:44:27 -0400

    removed boilerplate stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
